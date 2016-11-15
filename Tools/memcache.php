465098e5f9813935c6b35ffd539af96495e52289
Luuray <luuray@rainhan.com>
2016-11-15 21:33:29 +0800

    tested memcache manage tool

--- Tools/memcache.php
+++ Tools/memcache.php
@@ -4,7 +4,7 @@
  *
  * Memcached Manager
  *
- * @author $Author$
+ * @author $Author: Luuray $
  * @copyright Rainhan System
  * @date $Date$
  *
@@ -16,16 +16,14 @@
  * @version $Rev$
  */
 
-$aConfig = [
-	'Server' => '127.0.0.1',
-	'Port'   => 11211
-];
+$aServer['main'] = '127.0.0.1:11211';
+$aServer['second'] = 'unix://tmp/memcached.sock';
 
-$sAct = empty($_GET['act']) ? 'index' : $_GET['act'];
-if ($sAct == 'index') {
+$sAct = empty($_GET['act'])?'index':$_GET['act'];
+if($sAct == 'index'){
 	?>
-	<!DOCTYPE html>
-	<html lang="zh-cn" ng-app="memCache">
+<!DOCTYPE html>
+<html lang="zh-cn" ng-app="memCache">
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,25 +43,46 @@ if ($sAct == 'index') {
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		<style type="text/css">
+			table{
+				table-layout: fixed;
+				word-wrap: break-word;
+			}
+		</style>
 	</head>
 	<body>
 	<div class="container-fluid" ng-controller="IndexController">
 		<div class="alert alert-warning" role="alert" ng-show="hasAlert">{{message}}</div>
 		<div ng-controller="ListController">
-			<table class="table table-striped table-hover table-condensed" ng-repeat="(k, v) in servers">
-				<caption>Server: <span class="text-info">{{ k }}</span>,
-					Total Items: <span class="text-info">{{ v.curr_items }}</span>,
-					Size: <span class="text-info">{{ v.limit_maxbytes / 1048576.0 | number:2 }} MByte</span></caption>
-				<thead>
-				<tr>
-					<td>Key</td>
-					<td>Value</td>
-					<td class="danger">Delete <i class="glyphicon glyphicon-warning-sign"></i></td>
-				</tr>
-				</thead>
-				<tbody id="list">
-				</tbody>
-			</table>
+			<div ng-repeat="(k, v) in servers">
+				<table ng-if="v.flag == true" class="table table-striped table-hover table-condensed table-bordered">
+
+					<caption>Server: <span class="text-info">[{{ k }}]{{ v.connection }}</span>,
+						Total Items: <span class="text-info">{{ v.data.curr_items }}</span>,
+						Size: <span class="text-info">{{ v.data.bytes/1048576.0 |number:2}} MByte of {{ v.data.limit_maxbytes / 1048576.0 | number:2 }} MByte</span>,
+						Hit Rate: <span class="text-info">{{ (v.data.get_hits/(v.data.get_hits*1 + v.data.get_misses*1))*100 |number: 4 }} %</span>,
+						Next Refresh In <span class="text-danger">{{ refreshInterval/1000 }}</span> Seconds.
+					</caption>
+					<thead>
+					<tr>
+						<td class="col-xs-1">Key</td>
+						<td>Value</td>
+						<td class="danger col-xs-1">Delete <i class="glyphicon glyphicon-warning-sign"></i></td>
+					</tr>
+					</thead>
+					<tbody ng-controller="GetItemsController" ng-init="loadItems(k)">
+						<tr ng-if="loadSuccess" ng-repeat="item in items">
+							<td ng-bind="item.key"></td>
+							<td ng-bind="item.value"></td>
+							<td class="danger"><a href="#" ng-click="del($index, item.key)"><i class="glyphicon glyphicon-remove"></i> Delete</a></td>
+						</tr>
+						<tr ng-if="!loadSuccess">
+							<td colspan="3" class="warning">{{ message }}</td>
+						</tr>
+					</tbody>
+				</table>
+				<p ng-if="v.flag==false" class="text-warning">Server [{{k}}]{{ v.connection }}, Error: {{v.message}}</p>
+			</div>
 		</div>
 	</div>
 	<script type="text/javascript">
@@ -78,60 +97,204 @@ if ($sAct == 'index') {
 				})
 			}])
 
-			.controller('ListController', ['$scope', '$http', function ($scope, $http) {
-				$scope.servers = [];
+			.controller('ListController', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+				var refreshInterval = 30000;
+				var refreshTimer = null;
 
-				$http.get('?act=stats')
-					.success(function (data, status) {
-						if (data.status == false) {
-							$scope.$broadcast('triggerError', data.message)
-						}
-						else {
-							$scope.servers = data.data;
-						}
-					});
+				$scope.servers = [];
+				$scope.refreshInterval = refreshInterval;
+
+				var loader = function () {
+					refreshTimer == null ? null : $interval.cancel(refreshTimer);
+					$scope.refreshInterval = refreshInterval;
+					$scope.servers = [];
+
+					$http.get('?act=stats', {timeout: 3000})
+						.then(function (response) {
+							//timer
+							refreshTimer = $interval(function(){
+								$scope.refreshInterval -= 1000;
+							}, 1000);
+
+							$scope.servers = response.data;
+						}, function (error) {
+							alert(error.data);
+						});
+				};
+
+				loader();
+
+				$interval(loader, refreshInterval);
 			}])
+			.controller('GetItemsController', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+				$scope.server = '';
+				$scope.items = null;
+				$scope.loadSuccess = false;
+				$scope.message = '';
+
+				$scope.loadItems = function (server) {
+					$scope.server = server;
+
+					$scope.refreshItems();
+				};
+
+				$scope.refreshItems = function () {
+					$http.post('?act=list', {server: $scope.server}, {timeout: 3000})
+						.then(function (response) {
+							$scope.items = response.data;
+							$scope.loadSuccess = true;
+						}, function (error) {
+							$scope.loadSuccess = false;
+							$scope.message = error.data;
+						})
+				};
+
+				$scope.del = function (index, key) {
+					$http.post('?act=del', {server:$scope.server, key: key}, {timeout: 3000})
+						.then(function (response) {
+							$scope.items.splice(index, 1);
+						}, function (error) {
+							alert(error.data);
+						});
+					$scope.items.splice(index, 1);
+				}
+			}])
+
 	</script>
 	</body>
-	</html>
-	<?php
+</html>
+<?php
 	exit;
 }
 
 try {
-	if (class_exists('Memcached')) {
-		$oMemcache = new \Memcached();
+	@header_remove();
+	@header('Content-Type: application/json; charset=UTF-8');
+	if($sAct == 'stats'){
+		$aResult = [];
+		foreach($aServer AS $_name => $_srv){
+			$oServer = getMemcache($_srv);
 
-		if (substr($aConfig['Server'], 0, 7) == 'unix://') {
-			$bConnected = $oMemcache->addserver($aConfig['Server'], 0);
-		} else {
-			$bConnected = @$oMemcache->addserver(
-				$aConfig['Server'],
-				$aConfig['Port'] == null ? 11211 : intval($aConfig['Port']));
+			$aStats = $oServer->getStats();
+			$aStats = array_shift($aStats);
+
+			if($aStats['pid'] != -1){
+				$aResult[$_name]=['connection'=>$_srv,'flag'=>true, 'data'=>$aStats];
+			}
+			else {
+				$aResult[$_name] = ['connection'=>$_srv, 'flag'=>false, 'message'=>'no_connect'];
+			}
 		}
-	} else if (class_exists('Memcache')) {
-		$oMemcache = new \Memcache();
+		echo json_encode($aResult);
+		exit;
+	}
+	if($sAct == 'list'){
+		$aRequest = @file_get_contents('php://input');
+		$oResult = @json_decode($aRequest);
 
-		if (substr($aConfig['Server'], 0 ,7) == 'unix://') {
-			$bConnected = $oMemcache->connect($aConfig['Server'], 0);
-		} else {
-			$bConnected = @$oMemcache->connect(
-				$aConfig['Server'],
-				$aConfig['Port'] == null ? 11211 : intval($aConfig['Port']));
+		if($oResult == false){
+			@header('invalid_request', true, 500);
+			exit;
+		}
+
+		$sServer = array_key_exists($oResult->server, $aServer) ? $aServer[$oResult->server] : null;
+		if($sServer == null){
+			@header('undefined_server', true, 500);
+			exit;
+		}
+
+		$oServer = getMemcache($sServer);
+		if($oServer == false){
+			@header('no_connect', true, 500);
+			exit;
+		}
+
+		$aResult = [];
+
+		if($oServer instanceof Memcache) {
+			$aSlabs = $oServer->getExtendedStats('slabs');
+			$aItems = $oServer->getExtendedStats('items');
+			foreach ($aSlabs AS $_slab) {
+				foreach ($_slab AS $_sid => $_meta) {
+					if (!is_int($_sid)) continue;
+
+					$aDumps = $oServer->getExtendedStats('cachedump', (int)$_sid, 65535);
+
+					foreach ($aDumps AS $_srv => $_entries) {
+						if ($_entries) {
+							foreach ($_entries AS $_key => $_meta) {
+								$aResult[] = ['key' => $_key, 'value' => $oServer->get($_key)];
+							}
+						}
+					}
+				}
+			}
+		}
+		else{
+			$aKeys = $oServer->getAllKeys();
+			foreach($aKeys AS $_key){
+				$aResult[] = ['key'=>$_key, 'value'=>$oServer->get($_key)];
+			}
+		}
+
+		echo json_encode($aResult);
+		exit;
+	}
+	if($sAct == 'del'){
+		$aRequest = @file_get_contents('php://input');
+		$oResult = @json_decode($aRequest);
+
+		if($oResult == false){
+			@header('invalid_request', true, 500);
+			exit;
+		}
+
+		$sServer = array_key_exists($oResult->server, $aServer) ? $aServer[$oResult->server] : null;
+		if($sServer == null){
+			@header('undefined_server', true, 500);
+			exit;
+		}
+
+		$oServer = getMemcache($sServer);
+		if($oServer == false){
+			@header('no_connect', true, 500);
+			exit;
+		}
+
+		if($oServer->delete($oResult->key)){
+			echo json_encode(['message'=>'success']);
+		}
+		else{
+			@header('delete_fail', true, 500);
+			exit;
 		}
 	}
+}catch(Exception $ex){
+	//echo json_encode(['status' => 'error', 'message' => $ex]);
+	@header($ex->getMessage(), true, 500);
+}
 
-	if ($sAct == 'stats') {
-		echo json_encode(['status' => 'success', 'data' => (
-		($oMemcache instanceof Memcached) ?
-			$oMemcache->getStats() : $oMemcache->getextendedstats())]);
-	}
-	if ($sAct == 'list') {
+/**
+ * @param $sConn
+ *
+ * @return Memcache
+ */
+function getMemcache($sConn)
+{
+	if (substr($sConn, 0, 7) == 'unix://') {
+		$sServer = $sConn;
+		$iPort   = null;
+	} else {
+		$sServer = parse_url($sConn, PHP_URL_HOST);
+		$iPort   = parse_url($sConn, PHP_URL_PORT);
 
+		$sServer = $sServer == null ? 'localhost' : $sServer;
+		$iPort   = $iPort == null ? 11211 : $iPort;
 	}
-	if ($sAct == 'del') {
 
-	}
-} catch (Exception $ex) {
-	echo json_encode(['status' => 'error', 'message' => $ex]);
+	$oMemcache = class_exists('Memcached')?new Memcached():new Memcache();
+
+	$oMemcache->addServer($sServer, $iPort);
+
+	return $oMemcache;
 }
